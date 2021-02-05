aec8a426662f2ce5b81a98cadb5ecc64206f83be
duhaoyuan1111 <55334036+duhaoyuan1111@users.noreply.github.com>
2021-02-04 20:41:16 -0600

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -18,12 +18,35 @@ int main(int argc, char **argv)
 {
     Student student;
     double average;
-
+    double howmany;
+    student.f_name = new char[128];
+    student.l_name = new char[128];
     // Sequence of user input -> store in fields of `student`
 
-    // Call `CalculateStudentAverage(???, ???)`
-    // Output `average`
+    std::cout << "Please enter the student's id number: ";
+    std::cin >> student.id;
+    std::cin.ignore();
+    std::cout << "Please enter the student's first name: ";
+    std::cin.getline(student.f_name, 128);
+    std::cout << "Please enter the student's last name: ";
+    std::cin.getline(student.l_name, 128);
+    std::cout << "Please enter how many assignments were graded: ";
+    std::cin >> student.n_assignments;
+    howmany = student.n_assignments;
 
+    student.grades= new double[student.n_assignments];
+    int i;
+    for (i = 0; i < student.n_assignments; i++){
+        std::cout << "Please enter grade for assignment " << i << ":";
+        std::cin >> student.grades[i];
+    }
+    // Call `CalculateStudentAverage(???, ???)`
+    double *avgpointer = &howmany;
+
+    calculateStudentAverage(student.grades, avgpointer);
+    // Output `average`
+    std::cout << "Student: " << student.f_name << " " << student.l_name << " [" << student.id << "]";
+    std::cout << "  Average grade: " << average;
     return 0;
 }
 
@@ -54,4 +77,10 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    int i;
+    int sum = 0;
+    for (i = 0; i < *avg; i++){
+        sum += object;
+    }
+    *avg = sum;
 }
